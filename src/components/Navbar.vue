bb117184013d0f97ef42d4ec617becbd29a0725b
Philip Loa <boyet007@gmail.com>
2020-05-28 23:35:32 +0700

    navigation drawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <v-navigation-drawer dark class="primary" v-model="drawer" app>
+            <v-list dense>
+                <v-list-item router :to="link.route" v-for="(link, index) in links" :key="index">
+                <v-list-item-action >
+                    <v-icon>{{ link.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{ link.text }}</v-list-item-title>
+                </v-list-item-content>
+                </v-list-item>
+        </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app flat>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Todo Ninja</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                    <v-icon>mdi-logout-variant</v-icon>
+            </v-btn>
+        </v-app-bar>
+  </nav>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+
+    data: () => ({
+      drawer: true,
+      links: [
+          { icon: 'mdi-view-dashboard', text: 'Dashboard', route:'/' },
+          { icon: 'mdi-folder', text: 'My Projects', route:'/projects' },
+          { icon: 'mdi-account', text: 'Team', route:'/team' },
+      ]
+    }),
+  }
+</script>
