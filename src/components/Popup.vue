159c0f0e66e4b133175a1f5fcd1d4a19c0028c23
Philip Loa <boyet007@gmail.com>
2020-05-29 19:33:37 +0700

    Datepickers

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
-        <v-btn @click.stop="dialog=true" color="success">Add new project</v-btn>
-        <v-dialog width="600px" v-model="dialog">
+        <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on }">
+                <v-btn color="success" dark v-on="on">
+                    Add a new project
+                </v-btn>
+            </template>
             <v-card>
-               <v-card-title><h2>Add a New Project</h2></v-card-title>
-               <v-card-text>
+                <v-card-title><h2>Add a New Project</h2></v-card-title>
+                <v-card-text>
                    <v-form class="px-3">
                        <v-text-field label="Title" prepend-icon="folder" v-model="title"></v-text-field>
                         <v-textarea label="Information" prepend-icon="edit" v-model="content"></v-textarea>
+                       <v-menu  min-width="0">
+                            <template v-slot:activator="{ on }">
+                            <v-text-field :value="formattedDate" v-on="on" label="Due date" prepend-icon="date_range"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="due"></v-date-picker>
+                        </v-menu>
                         <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
                    </v-form>
                 </v-card-text>
@@ -17,17 +27,24 @@
 </template>
 
 <script>
+import { parseISO, format } from 'date-fns';
 export default {
     data() {
         return {
             dialog: false,
             title: '',
-            content: ''
+            content: '',
+            due: ''
         }
     },
     methods: {
         submit() {
             console.log(this.title, this.content)
+        }
+    },
+    computed: {
+        formattedDate() {
+            return this.due ? format(parseISO(this.due), 'do MMM yyyy') : '';
         }
     }
 }
