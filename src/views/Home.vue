619cd1cd3ed356476579d0634f00421f61b63f3e
Philip Loa <boyet007@gmail.com>
2020-05-29 13:49:46 +0700

    cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,23 @@
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
       <v-layout row class="mb-3">
-        <v-btn small text color="grey" @click="sortBy('title')">
-          <v-icon left small>mdi-folder</v-icon><span class="text-lowercase">By project name</span>
-        </v-btn>
-        <v-btn small text color="grey" @click="sortBy('person')">
-          <v-icon left small>mdi-account</v-icon><span class="text-lowercase">By person</span>
-        </v-btn>
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" v-on="on" @click="sortBy('title')">
+              <v-icon left small>mdi-folder</v-icon><span class="text-lowercase">By project name</span>
+            </v-btn> 
+          </template>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" v-on="on" @click="sortBy('person')">
+              <v-icon left small>mdi-account</v-icon><span class="text-lowercase">By person</span>
+            </v-btn>
+          </template>
+          <span>Sort project by person name</span>
+        </v-tooltip>
       </v-layout>
       <v-card flat v-for="(project, index) in projects" :key="index">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="team">
-    <h1 class="subheading grey--text">Teams</h1>
+    <h1 class="subheading grey--text">Team</h1>
+    <v-container class="my-5">
+      <v-layout row>
+        <v-flex xs12 sm6 md4 lg3 :key="person.name" v-for="person in team">
+          <v-card flat color="white" class="text-xs-center pa-4 ma-3">
+            <v-responsive>
+              image goes here
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="grey">
+                <v-icon small left>mdi-message-reply-text</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web Developer'},
+        { name: 'Ryu', role: 'Graphic designer'},
+        { name: 'Chun Li', role: 'Web Developer'},
+        { name: 'Dalsim', role: 'Social media maverick'},
+        { name: 'Yoshi', role: 'Sales guru'},
+      ]
+    }
+  }
 }
 </script>
